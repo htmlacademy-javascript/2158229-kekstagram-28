f06e3695a78a3d311c8ef38593536d10acdb5e78
Nikolai Didur <nadvoodoo@gmail.com>
2023-04-12 13:04:01 +0300

    вносит правки на защите проекта

--- js/big-photo.js
+++ js/big-photo.js
@@ -1,8 +1,9 @@
 const bigPhotoPreview = document.querySelector('.big-picture__preview');
 
 const renderBigPhoto = ({ url, description, likes }) => {
-  bigPhotoPreview.querySelector('.big-picture__img img').src = url;
-  bigPhotoPreview.querySelector('.big-picture__img img').alt = description;
+  const bigPhotoImg = bigPhotoPreview.querySelector('.big-picture__img img');
+  bigPhotoImg.src = url;
+  bigPhotoImg.alt = description;
   bigPhotoPreview.querySelector('.likes-count').textContent = likes;
   bigPhotoPreview.querySelector('.social__caption').textContent = description;
 };
